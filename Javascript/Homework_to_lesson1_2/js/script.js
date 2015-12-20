073881c3c528a8d1f3eb8471d67744e24081d6f7
ElenkaS <sonceuvikonci@mail.ru>
2015-12-20 23:59:50 +0200

    added changes to homework 1_2

--- Javascript/Homework_to_lesson1_2/js/script.js
+++ Javascript/Homework_to_lesson1_2/js/script.js
@@ -1,6 +1,13 @@
-/*Функция для возведения в степеью Результат выводится в консоль*/
+/*Функция для возведения в степень Результат выводится в консоль*/
 var x = +prompt('Введите основание степеня');
 var n = +prompt('Введите показатель степеня');
+
+if (Number.isInteger(x) && Number.isInteger(n)){
+	console.log(pow(x, n));
+} else {
+	console.log('Enter only integer numbers')
+}
+
 
 function pow(x, n) {
 	if (n >= 0) {
@@ -18,28 +25,41 @@ function pow(x, n) {
 		
 	return result;
 }
-console.log(pow(x, n));
 
 
 /*Массив с именами пользователей*/
 
 var arr = [];
 
-for ( var i = 0; i < 5; i++) {
-	arr[i] = prompt('Введите имя пользователя №'+(+i+1));
+for (var i = 0; i < 5; i++) {
+	var name =  prompt('Введите имя пользователя №'+(i+1));
+	if (name)
+		arr[i] = name;
 }
 
-var userName = prompt('Введите имя');
+if (arr.length > 0) {
+	var userName = prompt('Введите свое имя');
+	if (userName) {
+		var flag = false;
+		for (var i = 0; i < arr.length; i++) {
+			if (userName===arr[i]) {
+				flag = true;
+				break;
+			} 
+		}
 
-for (var i = 0; i < arr.length; i++) {
-	if (userName===arr[i]) {
-		var flag = true;
-		break;
-	} 
-}
-
-if (flag) {
-	alert(userName + ', вы успешно вошли');
-}	else {
-		alert('Ошибка входа');
+		if (flag) {
+			alert(userName + ', вы успешно вошли');
+		}	else {
+			alert('Ошибка входа');
+		}
+	} else {
+		alert('Вы не ввели своего имени');
 	}
+
+	
+} else {
+	alert('Вы не ввели ни одного имени');
+
+}
+
