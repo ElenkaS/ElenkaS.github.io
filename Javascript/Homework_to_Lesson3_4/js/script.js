0313293e89f61ff38c2d69b0281b86b5de46fc49
ElenkaS <sonceuvikonci@mail.ru>
2015-12-28 23:04:45 +0200

    edits by comments

--- Javascript/Homework_to_Lesson3_4/js/script.js
+++ Javascript/Homework_to_Lesson3_4/js/script.js
@@ -1,48 +1,133 @@
 var test = {
-	createTag: function(tagName, className, content, target, inputType) {
-		var element = document.createElement(tagName);
-		if (className) {
-			element.setAttribute('class', className);
+	body: '',
+	createTag: function(params) {
+		/*
+		params: {
+			tagName: '', 
+			className: '',
+			content: '',
+			target: '',
+			inputType: ''
 		}
-		if (content) {
-			element.innerHTML = content;
-		}
-		
-		if (inputType){
-      		element.setAttribute('type', inputType);
+*/
+		if (params.tagName == '') {
+			alert('Not set tagName!');
+			return;
 		}
 
+		if (params.tagName == 'textNode') {
+			return this.createTextNode(params.content, params.target);
+		}
+
+		var element = document.createElement(params.tagName);
+		if (params.className) {
+			element.setAttribute('class', params.className);
+		}
+		if (params.content) {
+			element.innerHTML = params.content;
+		}
+		
+		if (params.inputType){
+      		element.setAttribute('type', params.inputType);
+		}
+		var target = params.target;
 		if (!target) {
-			target = document.querySelector('body');
+			target = this.body;
 		} 
 		target.appendChild(element);
 
 		return element;
+	},
+	createTextNode: function (text, target) {
+		if (!text) {
+			alert ('Not set text for text node');
+			return;
+		}
+		var elem = document.createTextNode(text);
+		target.appendChild(elem);
+
+		return elem;
+	},
+	createOneAnswersBlock: function(params) {
+		/*
+		params: {
+					answersCount: '',
+					answerText: '',
+					target: ''
+				 }
+		*/
+		for (var i = 1; i <= params.answersCount; i++) {
+			var div = this.createTag({
+									tagName: 'div', 
+									className: 'offset50 checkbox',
+									target: params.target
+								})
+			var label = this.createTag({
+									tagName: 'label',
+									//content: '<input type="checkbox"/>'+params.answerText+i,
+									target: div,
+								})
+			var checkbox = this.createTag({
+									tagName: 'input',
+									target: label,
+									inputType: 'checkbox'
+								})
+			var label_text = this.createTag({
+									tagName: 'textNode',
+									target: label,
+									content: params.answerText+i
+								})
+			label.insertBefore(checkbox, label_text);
+			
+		};	
+	},
+	createAnketa: function(params){
+		/*
+		params: {
+					questionCount: '', 
+					questionText: '',
+					answersCount: '',
+					answerText: '',
+					target: ''
+				}
+		*/
+		for (var i = 1; i <= params.questionCount; i++) {
+			this.createTag({
+							tagName: 'h3', 
+							className: 'offset50',
+							content: i + '. ' + params.questionText + i,
+							target: params.target
+						})
+			this.createOneAnswersBlock({
+										answersCount: params.answersCount,
+										answerText: params.answerText,
+										target: params.target
+									 })
+		}
 	}
 }
 
-test.createTag('h2', 'text-center', 'Тест по программированию');
-var form = test.createTag('form', '', '');
+//=====================================начало программы=============================
 
-test.createTag('h3', 'offset50', '1. Вопрос №1', form);
-for (var i = 1; i <= 3; i++) {
-	div = test.createTag('div', 'offset50 checkbox', '', form);
-	label = test.createTag('label', '', '<input type="checkbox"/>Вариант ответа №'+i, div);
-	//test.createTag('input', '', '', label, 'checkbox');
-};
+test.body = document.querySelector('body');
+test.createTag({
+				tagName: 'h2', 
+				className: 'text-center',
+				content: 'Тест по программированию'
+			});
 
-test.createTag('h3', 'offset50', '2. Вопрос №2', form);
-for (var i = 1; i <= 3; i++) {
-	div = test.createTag('div', 'offset50 checkbox', '', form);
-	label = test.createTag('label', '', '<input type="checkbox"/>Вариант ответа №'+i, div);
-	//test.createTag('input', '', '', label, 'checkbox');
-};
+var form = test.createTag({tagName: 'form'});
+test.createAnketa({
+					questionCount: 3, 
+					questionText: 'Вопрос №',
+					answersCount: 3,
+					answerText: 'Вариант ответа №',
+					target: form
+				});
 
-test.createTag('h3', 'offset50', '3. Вопрос №3', form);
-for (var i = 1; i <= 3; i++) {
-	div = test.createTag('div', 'offset50 checkbox', '', form);
-	label = test.createTag('label', '', '<input type="checkbox"/>Вариант ответа №'+i, div);
-	//test.createTag('input', '', '', label, 'checkbox');
-};
-
-test.createTag('button', 'btn btn-primary center-block', 'Проверить мои результаты', form);
+test.createTag({
+				tagName: 'button', 
+				className: 'btn btn-primary center-block',
+				content: 'Проверить мои результаты',
+				target: form
+			})
